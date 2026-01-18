e87d1c60310c74ac7df7de3066d9c99b1a9f7419
fahrimert <fmtunckaya@gmail.com>
2026-01-19 00:53:01 +0300

    feat: add ec2 and security group

--- main.tf
+++ main.tf
@@ -52,3 +52,46 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-server-sg"
+  description = "Security wall for web server"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web Security Group"
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-df5de72ade3b42331" 
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.public_subnet.id
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Merhaba Terraform! Bu sunucu kod ile kuruldu , deneme 1 deneme 2 deneme 3." > index.html
+              python3 -m http.server 80 &
+              EOF
+
+  tags = {
+    Name = "Terraform-Learning-Web-Server"
+  }
+}
