fe6b9d94232bbe8ccb88ddedfec439cc6bad4450
fahrimert <fmtunckaya@gmail.com>
2026-01-19 00:20:15 +0300

    feat: remove s3 and implement network layer

--- main.tf
+++ main.tf
@@ -1,8 +1,54 @@
-resource "aws_s3_bucket" "terraform-learning-local-bucket" {
-  bucket = "terraform-learning-local-bucket"
-  
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
-    Name        = "Terraform Learning Local Bucket"
-    Environment = "Dev"
+    Name = "terraform-learning-lab-vpc"
   }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24" 
+  map_public_ip_on_launch = true          
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Private Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "Main Gateway"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"              
+    gateway_id = aws_internet_gateway.gw.id 
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
 }
